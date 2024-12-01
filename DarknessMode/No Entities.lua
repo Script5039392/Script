a1bb34d55893a9afc6d67d59fb73095f0dcb1744
Script5039392 <outtestacc8@gmail.com>
2024-12-01 12:50:05 +0700

    Update No Entities.lua

--- DarknessMode/No Entities.lua
+++ DarknessMode/No Entities.lua
@@ -452,3 +452,27 @@ coroutine.wrap(function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ChronoAcceleration/Hotel-Plus-Plus/main/Entities/BaseEntityRemux.lua"))()
 
 end)()
+
+
+------------ Door
+
+coroutine.wrap(function()
+
+local ScriptUtility = loadstring(game:HttpGet("https://raw.githubusercontent.com/ChronoAcceleration/Comet-Development/refs/heads/main/Doors/Utility/DoorsScriptUtility.lua"))()
+local RoomHook = ScriptUtility.RoomHook:New()
+
+RoomHook:On(
+    "ServerRoomChanged",
+    function(Room: Model): ()
+        Room:WaitForChild("Door")
+        
+        local Door = Room:WaitForChild("Door")
+        Door.Door.Unlock.SoundId = "rbxassetid://7758131110"
+        Door.Door.Bell.Volume = 0.95
+        Door.Door.SlamOpen.SoundId = "rbxassetid://9113647192"
+        Door.Door.SlamOpen.TimePosition = 0.2
+        Door.Door.Fall.Volume = 0.5
+    end
+		)
+
+end)()
