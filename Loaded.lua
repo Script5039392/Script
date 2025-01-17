24fa9262401b2c9f63450e7662a6a030e8d49164
Script5039392 <outtestacc8@gmail.com>
2025-01-17 21:06:36 +0700

    Update Loaded.lua

--- Loaded.lua
+++ Loaded.lua
@@ -1,53 +1,5 @@
-caa = 0
-tween = game:GetService("TweenService")
-local TestMultplayer = true
-if game:GetService("ReplicatedStorage"):FindFirstChild("Oneeee") then
-    print("?")
-    return true
-end
-
-local Test = Instance.new("Part")
-Test.Name = "Oneeee"
-Test.Parent = game:GetService("ReplicatedStorage")
-Test = 1
-
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Darkness Mode", HidePremium = false, SaveConfig = false, ConfigFolder = "Dark"})
-
-local Tab = Window:MakeTab({
-	Name = "Select",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-
-Tab:AddButton({
-	Name = "No entity",
-	Callback = function()
-      		OrionLib:Destroy()
-              wait()
-              loadstring(game:HttpGet("https://raw.githubusercontent.com/Script5039392/Script/refs/heads/main/DarknessMode/No%20Entities.lua"))()
-  	end    
-})
-
-Tab:AddButton({
-	Name = "Add Entity",
-	Callback = function()
-      		OrionLib:Destroy()
-              wait()
-              loadstring(game:HttpGet("https://raw.githubusercontent.com/Script5039392/Script/refs/heads/main/DarknessMode/Entities.lua"))()
-  	end    
-})
-
-game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
-
------------- Error
-
-coroutine.wrap(function()
-
-if not game:GetService("ReplicatedStorage"):FindFirstChild("Ob") then
-
-game.Players.LocalPlayer:Kick("Wth Error")
-
-end
-
-end)()
+require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("Mode Has Shutdown, Sorry You",true)
+wait(2.6)
+require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("You can test new mode ",true)
+wait(1)
+setclipboard("loadstring(game:HttpGet('https://pastefy.app/YAx1dSmY/raw'))() ")
